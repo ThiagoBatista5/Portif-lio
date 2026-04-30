b2c10fab2464eb8382e2654c0750eab8a7a3d094
ThiagoBatista5 <thiagodromont22@gmail.com>
2026-04-30 20:22:30 -0300

    modified

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const navbar = document.getElementById('navbar');
 window.addEventListener('scroll', () => {
   navbar.classList.toggle('scrolled', window.scrollY > 40);
 });
+const btn = document.querySelector('.back');
 
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 100){
+    btn.classList.add('visivel');
+  }else {
+    btn.classList.remove('visivel');
+  };
+});
 // ── REVEAL ON SCROLL: anima elementos ao entrar na viewport ──
 const reveals = document.querySelectorAll('.reveal');
 
